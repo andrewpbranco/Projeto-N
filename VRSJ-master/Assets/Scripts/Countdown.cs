3f0ed2516894a709d004f7a336626b7e4feac614
andrewpbranco <58531866+andrewpbranco@users.noreply.github.com>
2020-05-08 21:27:44 -0300

    Main Commit

---
+++ VRSJ-master/Assets/Scripts/Countdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    public float count = 0;
+    private float currentcount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentcount = count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentcount -= Time.deltaTime * 1;
+
+        if (currentcount <= 0)
+        {
+            SceneManager.LoadScene("Menu");
+		}
+    }
+}
---
+++ VRSJ-master/Assets/Scripts/FPSWalk.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FPSWalk : MonoBehaviour
+{
+    public AudioSource steps;
+    public CharacterController character;
+    public Vector3 positionToGo;
+    float counttodie=5;
+    public AudioSource warning;
+ // public GameObject LookBackCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionToGo = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diftowalk = positionToGo - transform.position;
+
+        character.SimpleMove(diftowalk.normalized);
+       
+        steps.volume = diftowalk.magnitude-1;      
+    }
+
+ /*   private void OnTriggerEnter(Collider other)
+    {
+          if (other.CompareTag("Walkable"))
+        {
+             LookBackCollider.SetActive();
+        }
+    }*/
+   /* private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Walkable"))
+        {
+            GameObject.destroy();
+
+        }
+    }*/
+}
